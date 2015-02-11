950bf5c1dbb985024a066cc7741ea2eb7a206cda
jimsbarz <jimsbarz@github.com>
2015-02-11 02:45:59 -0500

    changes

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-FooCoin (FOO)
+TestBars (TSB)
 ===========
 
-[![Build Status](https://travis-ci.org/RazorLove/foocoin.png?branch=master)](https://travis-ci.org/RazorLove/foocoin)
+[![Build Status](https://travis-ci.org/RazorLove/testbars.png?branch=master)](https://travis-ci.org/RazorLove/testbars)
 
 
 Scrypt Hashcash PoW Template
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("TestBars");
+    case mBTC: return QString("milliTestBars (1 / 1,000)");
+    case uBTC: return QString("microTestBars (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of TestBars is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
