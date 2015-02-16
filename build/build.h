9dd9fdf9079cd872a83ab8b99acc053678a26b76
testbars <t.bars@email.com>
2015-02-15 23:50:21 -0500

    changes

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2015-02-15 15:53:49 -0500"
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("TBS");
+    case mBTC: return QString("mTBS");
+    case uBTC: return QString::fromUtf8("μTBS");
     default: return QString("???");
     }
 }
